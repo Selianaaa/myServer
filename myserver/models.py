7be602b62546eabcf6aa76b77d85d1bbbac433d5
Seliana <cany96@bk.ru>
2019-09-13 23:50:07 +0300

    models code improved

--- myserver/models.py
+++ myserver/models.py
@@ -1,15 +1,17 @@
-from django.db import models
 from django.contrib.postgres.fields import ArrayField
+
+from django.db import models
+
 
 class Item(models.Model):
     id = models.UUIDField(primary_key=True)
     created = models.DateTimeField(auto_now_add=True)
     image_vector = ArrayField(
         models.FloatField(),
-        null=True
+        null=True,
     )
     image_width = models.IntegerField(null=True)
-    
+
 
     class Meta:
         db_table = 'items'
